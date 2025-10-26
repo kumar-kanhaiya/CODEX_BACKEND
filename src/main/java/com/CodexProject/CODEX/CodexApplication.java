07307e9512a5aa6a6f68ca7de74ae77890b21dd6
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-10-26 11:20:15 +0530

    Initial commit

---
+++ src/main/java/com/CodexProject/CODEX/CodexApplication.java
@@ -0,0 +1,13 @@
+package com.CodexProject.CODEX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodexApplication.class, args);
+	}
+
+}
