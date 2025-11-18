6db8b02cff77666f536da801633322dcd7ae1767
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-11-18 17:27:59 +0530

    code

--- src/main/java/com/CodexProject/CODEX/config/WebConfig.java
+++ src/main/java/com/CodexProject/CODEX/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.CodexProject.CODEX.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${frontend.url}")
+    private String frontendLink;
+
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173/")
+                .allowedOrigins(frontendLink)
                 .allowedMethods("GET","POST","UPDATE","DELETE","OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
